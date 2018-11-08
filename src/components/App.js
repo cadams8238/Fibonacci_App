8fb329bd45ca3b371f66064542aac0fff6e76457
Courtney Adams <cadams8238@gmail.com>
2018-11-08 15:06:33 -0800

    added content to App component explaining to the user what to do with the app

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,16 @@ import React, { Component } from "react";
 
 class App extends Component {
 	render() {
-		return <div>hello world</div>;
+		return (
+			<div>
+				<h1>Fibonacci Sequence</h1>
+				<p>
+					Enter a number below and see what numbers make up the Fibonacci
+					sequence up to that number. (Please note the sequence will start at 0)
+				</p>
+				<p>Example: If you input 5, you'll see 0 1 1 2 3</p>
+			</div>
+		);
 	}
 }
 
