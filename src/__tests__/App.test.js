0aa7b720dd93e18e98b4b7a4733b1e8afb27a639
Courtney Adams <cadams8238@gmail.com>
2018-11-08 21:01:12 -0800

    added tests for App component

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,9 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import App from "../components/App";
+import InputNumberForm from "../components/InputNumberForm";
+import { mount } from "enzyme";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("<App />", () => {
+	describe("rendering", () => {
+		let wrapper;
+
+		beforeEach(() => {
+			wrapper = mount(<App />);
+		});
+
+		it("Should render without crashing", () => {
+			// eslint-disable-next-line no-unused-expressions
+			wrapper;
+		});
+
+		it("Should render a form", () => {
+			expect(wrapper.find("form")).toHaveLength(1);
+		});
+
+		it("Should contain a h1 element", () => {
+			expect(wrapper.find("h1")).toHaveLength(1);
+		});
+
+		it("Should contain a couple p elements", () => {
+			expect(wrapper.find("p")).toHaveLength(2);
+		});
+
+		it("Should contain a InputNumberForm element", () => {
+			expect(wrapper.find("InputNumberForm")).toHaveLength(1);
+		});
+
+		describe("props", () => {
+			it("Should not receive props", () => {
+				expect(Object.keys(wrapper.props()).length).toBe(0);
+			});
+		});
+	});
 });
